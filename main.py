3e4fb7d8d107464d82e5d55deaaae7fcfc86fa52
e1923017 <e1923017@oit.ac.jp>
2026-01-13 18:00:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import datetime
 import random
 
+# 追加　スプレッドシート参照
 CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmjJQCY8tTaIYkXaK90sgZCnaZE-vvsfGEfg4_o1VFl1dpnSp3yl5g1z2-PCfqzk6vNm6tzYen3fC4/pub?output=csv"
 
 st.set_page_config(page_title="空き状況確認", layout="wide")
