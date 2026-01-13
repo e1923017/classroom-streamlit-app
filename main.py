d870fddffd5fdf395cac229e8bb7846b183f9a7b
e1923017 <e1923017@oit.ac.jp>
2026-01-13 18:09:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,23 +11,13 @@ st.set_page_config(page_title="空き状況確認", layout="wide")
 
 st.title("空き状況確認アプリ")
 
-#def load_data():
-#    now = datetime.datetime.now()
-#    data = {
-#        "time": [now - datetime.timedelta(minutes=5*i) for i in range(10)][::-1],
-#        "people": [random.randint(0, 30) for _ in range(10)]
-#    }
-#    return pd.DataFrame(data)
-
-#df = load_data()
-
 # スプレッドシート参照
 df = pd.read_csv(CSV_URL)
 
 # 人数取得（スプレッドシート参照）
 count = df[df["教室"] == room]["人数"].values[0]
 
-st.metric("現在の人数", {count}.)
+st.metric("現在の人数", {count})
 
 
 
